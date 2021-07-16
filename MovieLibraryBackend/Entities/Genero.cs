4640b2984b21ff9533d265a45d4f0e370cbdf0a1
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-16 17:50:02 -0500

    creating the first API class

---
+++ MovieLibraryBackend/Entities/Genero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLibraryBackend.Entities
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
