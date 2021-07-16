1c51abb91dd455afdf408634fab66c6b5c5d0fce
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-16 17:58:08 -0500

    probando salida de string por api

---
+++ MovieLibraryBackend/Controllers/GenerosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLibraryBackend.Controllers
+{
+    public class GenerosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("inicio")]
+        public string getAll()
+        {
+            string mensaje = "testing";
+
+            
+            return mensaje;
+        }
+    }
+}
